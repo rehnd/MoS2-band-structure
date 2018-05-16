54fdc14708127de9a44c0ab5a46968255bdaa416
Danny Rehn <danny.rehn@gmail.com>
2018-05-16 07:35:46 -0700

    latest changes to espresso phonon calculations

--- espresso/phonon/2-scf/plotdos.py
+++ espresso/phonon/2-scf/plotdos.py
@@ -2,10 +2,10 @@ import os
 from pylab import *
 rcParams.update({'font.size': 36, 'text.usetex': True})
 
-#os.system("/home/rehnd/.local/qe-6.2.1/bin/dos.x < mote2.dos.in")
+os.system("/home/rehnd/.local/qe-6.2.1/bin/dos.x < mos2.dos.in")
 
-dos = genfromtxt("mote2.dos")
-efermi = 2.3896
+dos = genfromtxt("mos2.dos")
+efermi = 7.634
 
 figure(figsize=(6,12))
 plot(dos[:,1], dos[:,0]-efermi,lw=4)
