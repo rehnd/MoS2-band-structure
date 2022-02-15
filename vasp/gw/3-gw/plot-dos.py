7a5431fe8a5c06bc608c14cdd2fb49912bbef6ea
Daniel Adam Rehn <rehnd@lanl.gov>
2022-02-15 12:49:14 -0700

    Add GW calculation

---
+++ vasp/gw/3-gw/plot-dos.py
@@ -0,0 +1,26 @@
+from pylab import *
+rcParams.update({'font.size': 48, 'text.usetex': True})
+
+f = open('DOSCAR')
+ef = float(f.readlines()[5].split()[3])
+f.close()
+
+dos = genfromtxt('DOSCAR', skip_header=6)
+
+f=figure(figsize=(14,12))
+plot(dos[:,0]-ef,dos[:,1], label='S GW')
+
+# f = open('s-potcar/DOSCAR')
+# ef = float(f.readlines()[5].split()[3])
+# f.close()
+
+#dos = genfromtxt('s-potcar/DOSCAR', skip_header=6)
+#plot(dos[:,0]-ef,dos[:,1],label='S')
+#legend()
+axhline(0,color='k',linewidth=2)
+axvline(0,color='k',linewidth=1)
+ylabel('DOS (states/eV/cell)')
+xlabel("Energy (eV)")
+#savefig('edos_mote2.png', bbox_inches='tight', dpi=200)
+tight_layout()
+show()
